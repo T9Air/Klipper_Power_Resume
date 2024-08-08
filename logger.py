7e35dd5990ca9ab11313f8b5598d2d6735a76b5f
T9Air <163940743+T9Air@users.noreply.github.com>
2024-08-07 20:37:39 -0400

    Fixed error with floats to strings not working

--- logger.py
+++ logger.py
@@ -1,16 +1,16 @@
 def increment_number(filename, x_position, y_position, z_position):
   try:
     with open(filename, 'r+') as file:
-      number = int(file.read() or 0)
+      number = int(file.readline())
       number += 1
       file.seek(0)
       file.truncate()
       file.write(str(number))
-      file.write("\n" + "G0 X" + x_position + " Y" + y_position + " Z" + z_position)
+      file.write("\n" + "G0 X" + str(x_position) + " Y" + str(y_position) + " Z" + str(z_position))
   except FileNotFoundError:
     with open(filename, 'w') as file:
       file.write('1')
-      file.write("\n" + "G0 X" + x_position + " Y" + y_position + " Z" + z_position)      
+      file.write("\n" + "G0 X" + str(x_position) + " Y" + str(y_position) + " Z" + str(z_position))      
 
 def clear_file(filename):
   with open(filename,'r+') as file:
