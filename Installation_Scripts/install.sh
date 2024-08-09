1fa44ab4ee0ac40de4189bb6cc0c8ea99f12b095
T9Air <163940743+T9Air@users.noreply.github.com>
2024-08-09 16:57:26 -0400

    create install.sh
    installation until changing files to match username

---
+++ Installation_Scripts/install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+clear
+echo "KLIPPER_POWER_RESUME Installer:"
+read -r -p "Are you sure you want to proceed? (y/N)" response1
+
+if [[ "$response1" == [Nn] ]]; then
+    echo "Exiting..."
+    exit 1
+fi
+echo " "
+echo "Please install the extended_macros repo."
+echo " "
+echo "This will download another repository from GitHub, and install it."
+echo " "
+echo "Please follow the instructions at this link: https://github.com/droans/klipper_extras/blob/main/extended_macro/readme.md"
+echo " "
+echo "Once you finish installing it, you will be sent back here"
+echo " "
+read -r -p "Do you want to install it from here? (Y/n)" response2
+echo " "
+
+if [[ "$response2" == [Yy] ]]; then
+    cd ~
+    git clone https://github.com/droans/klipper_extras.git
+    cd klipper_extras
+    python3 scripts/install.py
+else
+    echo "Please remember to install it on your own, otherwise this will not work."
+    echo " "
+fi
+
+read -r -p "Please input your username" response3
+
+sed -i 's/US
