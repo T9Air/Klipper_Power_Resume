ee75deecf67143f0c8c921c24a4ac77525aa4a64
T9Air <163940743+T9Air@users.noreply.github.com>
2024-08-08 23:30:48 -0400

    Update Make_Resumed_File.py
    make it easier to change bed & extruder temps

--- Make_Resumed_File.py
+++ Make_Resumed_File.py
@@ -56,7 +56,9 @@ def delete_lines(file1, file2, gcode):
 # Specify the log and g-code file paths and start g-code
 file1 = "/home/$USER/Klipper_Power_Resume/log.txt"
 file2 = "stopped file"
-gcode = "M190 S60\nG28\nM109 S200\nUNLOG_FILE"
+extruder_temp = "200"
+bed_temp = "60"
+gcode = "M190 S" + bed temp + "\nG28\nM109 S" + extruder_temp + "\nUNLOG_FILE"
 
 # Call the function to delete lines, add start g-code, and create a new g-code file
 delete_lines(file1, file2, gcode)
