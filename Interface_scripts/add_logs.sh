1a8c324c62863b5812d0fa7441087fd1f7d7e010
T9Air <163940743+T9Air@users.noreply.github.com>
2024-08-11 19:19:43 -0400

    Update add_logs.sh
    added error catching, and made num equal at least 1

--- Interface_scripts/add_logs.sh
+++ Interface_scripts/add_logs.sh
@@ -4,18 +4,28 @@ clear
 
 echo "Please write the name of the file you want to add."
 echo "If it is in a subdirectory, please write it in this format: directory/file."
-read -r -p "If you want to go back to the Main Menu, press 0 and then press enter" filepath
+echo "If you want to go back to the Main Menu, press enter"
+echo " "
+read -r -p "Please input the filename: " filepath
 
-if [[ "$filepath" == [0] ]]; then
+if [[ "$filepath" == "" ]]; then
     echo "Exiting..."
     exit 0
 fi
 
-num=0
+num=1
 
 filepath="/home/$USER/printer_data/gcodes/$filepath"
 
-read -r -p "How many lines do you want to skip between logs?" num
+if [[ ! -f "$filepath" ]]; then
+    echo "File not found: $filepath"
+    exit 1
+fi
+
+
+read -r -p "How many lines do you want to skip between logs? " num
+
+num=num+1
 
 sed -i "${num}~${num}a\LOG_FILE" $filepath
 
@@ -24,4 +34,3 @@ sed -i '1i \UNLOG_FILE' $filepath
 echo "File changed!"
 echo "Press any key to exit..."
 read -n1 -s
-exit 0
