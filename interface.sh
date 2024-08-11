9622624aa687fe107d598d19b211cf5da6f1267e
T9Air <163940743+T9Air@users.noreply.github.com>
2024-08-10 23:23:27 -0400

    Update interface.sh
    added basic gui, and linked to install script

---
+++ interface.sh
@@ -0,0 +1,15 @@
+clear
+
+echo "Klipper_Power_Resume:"
+echo "(1) Install"
+echo "(2) Add logging gcode to a file"
+echo "(3) Make a restarted file"
+echo "(4) Create start gcode"
+echo "(5) Edit start gcode"
+echo "(6) Uninstall"
+
+read -r -n1 -p "#### Perform an action: " action
+
+if [["$action" == 1]]; then
+    bash /home/$USER/Klipper_Power_Resume/Interface scripts/install.sh
+fi
