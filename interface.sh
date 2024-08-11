f513c32cf940e5af7d285d9f421b46a57232817f
T9Air <163940743+T9Air@users.noreply.github.com>
2024-08-11 15:20:56 -0400

    created script to add logs
    Added exit code, and connecton to script to add logs

--- interface.sh
+++ interface.sh
@@ -9,6 +9,7 @@ echo "(3) Make a restarted file"
 echo "(4) Create start gcode"
 echo "(5) Edit start gcode"
 echo "(6) Uninstall"
+echo "(0) Quit"
 
 read -r -n1 -p "#### Perform an action: " action
 
@@ -16,6 +17,15 @@ if [["$action" == 1]]; then
     bash /home/$USER/Klipper_Power_Resume/Interface scripts/install.sh
 fi
 
+if [["$action" == 2]]; then
+    bash /home/$USER/Klipper_Power_Resume/Interface scripts/add_logs.sh
+fi
+
 if [["$action" == 6]]; then
     bash /home/$USER/Klipper_Power_Resume/Interface scripts/uninstall.sh
+fi
+
+if [["$action" == 0]]; then
+    echo "exiting..."
+    exit 0
 fi
