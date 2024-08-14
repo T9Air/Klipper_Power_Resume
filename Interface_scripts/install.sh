6738c4e86bc79eef1927f6c51aa4942fefcf60ec
T9Air <163940743+T9Air@users.noreply.github.com>
2024-08-13 22:21:17 -0400

    Update install.sh
    delete renaming user in make resumed file python script, as it is uneseccary and has been moved to deprecated

--- Interface_scripts/install.sh
+++ Interface_scripts/install.sh
@@ -36,7 +36,6 @@ echo "Changing username in files to your username..."
 
 sed -i "s/\/USER\([[:alnum:]_]*\)/\/$USER\1/g" ~/Klipper_Power_Resume/logger.cfg
 sed -i "s/\/USER\([[:alnum:]_]*\)/\/$USER\1/g" ~/Klipper_Power_Resume/config.yaml
-sed -i "s/\/USER\([[:alnum:]_]*\)/\/$USER\1/g" ~/Klipper_Power_Resume/Make_Resumed_File.py
 
 echo "usernames changed"
 echo " "
