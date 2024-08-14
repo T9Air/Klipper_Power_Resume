1c77d47336263e46040c81f00e53415d3baec3f0
T9Air <163940743+T9Air@users.noreply.github.com>
2024-08-14 17:17:49 -0400

    Update edit_gcode.sh
    check whether there are files in start_gcode directory

--- Interface_scripts/edit_gcode.sh
+++ Interface_scripts/edit_gcode.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+clear
+
+if [ "$(ls -A "/home/$USER/Klipper_Power_Resume/start_gcode")" ]; then
+    echo "These are the files currently available to edit..."
+    echo " " 
+    cd /home/$USER/Klipper_Power_Resume/start_gcode
+    ls
+else
+    echo "Directory is empty..."
+    echo "Exiting..."
+    read -r -n1 -s
+    /home/$USER/Klipper_Power_Resume/interface.sh
+    exit 0
+fi
+
+
+
