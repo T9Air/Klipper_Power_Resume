83db422fe971463657e5f8a8b3936002babb58f7
T9Air <163940743+T9Air@users.noreply.github.com>
2024-08-18 14:55:49 -0400

    Update logger.py
    log layer

--- logger.py
+++ logger.py
@@ -35,6 +35,8 @@ def log(filename, x_position, y_position, z_position, layer):
             # Write the G0 command to a new line in the file
             file.write("\n" + movement_command)
 
+            file.write(str(layer))
+
     except FileNotFoundError:
         # If the file doesn't exist, create it and write initial values
         with open(filename, 'w') as file:
