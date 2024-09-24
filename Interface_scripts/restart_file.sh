d9dd8eda8ed82006e57823d966fef05573281ab3
T9Air <163940743+T9Air@users.noreply.github.com>
2024-09-24 16:57:39 -0400

    Update restart_file.sh

--- Interface_scripts/restart_file.sh
+++ Interface_scripts/restart_file.sh
@@ -3,3 +3,13 @@
 clear
 
 /home/$USER/Klipper_Power_Resume/Interface_scripts/make_restarted_file.sh restart
+
+clear
+
+read -r -p "Do you want to restart the print now? (y/N) " run
+
+if [ $run == [Yy] ]; then
+fi
+
+/home/$USER/Klipper_Power_Resume/interface.sh
+exit 0
