5f98ddc32b9317cf5a28f1f7a62bf889d620f037
T9Air <163940743+T9Air@users.noreply.github.com>
2024-09-24 17:07:52 -0400

    Update restart_file.sh
    send command to start print

--- Interface_scripts/restart_file.sh
+++ Interface_scripts/restart_file.sh
@@ -6,9 +6,15 @@ clear
 
 clear
 
+orig_filepath=$(sed -n '1p' /home/$USER/Klipper_Power_Resume/log.txt)
+origfilepath_no_extension="${orig_filepath%.*}"
+
+filename="${origfilepath_no_extension}_restarted.gcode"
+
 read -r -p "Do you want to restart the print now? (y/N) " run
 
 if [ $run == [Yy] ]; then
+    echo SDCARD_PRINT_FILE FILENAME=$filename > ~/printer_data/comms/klippy.serial
 fi
 
 /home/$USER/Klipper_Power_Resume/interface.sh
