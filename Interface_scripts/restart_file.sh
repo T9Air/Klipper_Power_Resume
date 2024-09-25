3dd460af45b0266a2679376e6dc7dfde571e4693
T9Air <163940743+T9Air@users.noreply.github.com>
2024-09-24 21:51:42 -0400

    Update restart_file.sh
    comment out the swich to make_res..., will do that in the script

--- Interface_scripts/restart_file.sh
+++ Interface_scripts/restart_file.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-clear
-
-/home/$USER/Klipper_Power_Resume/Interface_scripts/make_restarted_file.sh restart
-
-clear
-
-orig_filepath=$(sed -n '1p' /home/$USER/Klipper_Power_Resume/log.txt)
-origfilepath_no_extension="${orig_filepath%.*}"
-
-filename="${origfilepath_no_extension}_restarted.gcode"
-filename=$(basename filename)
+# clear
+# /home/$USER/Klipper_Power_Resume/Interface_scripts/make_restarted_file.sh restart
+# clear
+# orig_filepath=$(sed -n '1p' /home/$USER/Klipper_Power_Resume/log.txt)
+# origfilepath_no_extension="${orig_filepath%.*}"
+# filename="${origfilepath_no_extension}_restarted.gcode"
+# filename=$(basename filename)
 
 read -r -p "Do you want to restart the print now? (y/N) " run
 
