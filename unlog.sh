05a36ddd68b577a4a26f04ed576620d5ccad6b42
T9Air <163940743+T9Air@users.noreply.github.com>
2024-09-29 21:06:55 -0400

    Update unlog.sh

--- unlog.sh
+++ unlog.sh
@@ -2,5 +2,5 @@
 
 # Clear log.txt and log file path to it
 file_path=$1
-echo "$file_path" > /home/$USER/Klipper_Power_Resume/log.txt
-echo 0 >> /home/$USER/Klipper_Power_Resume/log.txt
+echo "$file_path" > /home/$USER/Klipper_Power_Resume/static_log.txt
+echo 0 > /home/$USER/Klipper_Power_Resume/dynamic_log.txt
