bb8368bf479d22d810674a6fe7da55d5ceada45b
T9Air <163940743+T9Air@users.noreply.github.com>
2024-09-29 21:07:30 -0400

    Update log.sh

--- log.sh
+++ log.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-log_path="/home/$USER/Klipper_Power_Resume/log.txt"
+log_path="/home/$USER/Klipper_Power_Resume/dynamic_log.txt"
 
-file_path=$(sed -n '1p' $log_path)
-
-linenumber=$(sed -n '2p' $log_path)
+linenumber=$(sed -n '1p' $log_path)
 
 linenumber=$((linenumber + 1))
 
@@ -17,9 +15,7 @@ speed=$(( speed * 60 ))
 
 truncate -s 0 $log_path
 
-echo $file_path > $log_path
-
-echo $linenumber >> $log_path
+echo $linenumber > $log_path
 
 echo $x >> $log_path
 echo $y >> $log_path
