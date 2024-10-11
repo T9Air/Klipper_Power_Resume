62f104b7becd5a417ff382b65c374871d66ce91b
T9Air <163940743+T9Air@users.noreply.github.com>
2024-10-11 13:04:36 -0400

    Update kpr.sh

--- kpr.sh
+++ kpr.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+echo "Klipper_Power_Resume v3.4.1"
+echo " "
+echo "Feedback would be greatly appreciated"
+echo " "
+read -r -n1 -s "Press enter to continue..."
+clear
+
 bash "/home/$USER/Klipper_Power_Resume/Interface_scripts/menu.sh" restart
 exit 0
