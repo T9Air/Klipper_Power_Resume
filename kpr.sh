1d73c74b3b35fe3ca950d9f7ecc298922816c3b3
T9Air <163940743+T9Air@users.noreply.github.com>
2024-10-15 22:36:21 -0400

    Update kpr.sh
    fix bug where script would not wait until keypress to continue

--- kpr.sh
+++ kpr.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+clear
+
 echo "Klipper_Power_Resume v3.4.1"
 echo " "
 echo "Feedback would be greatly appreciated"
 echo " "
-read -r -n1 -s "Press enter to continue..."
+echo "Press enter to continue..."
+
+read -r -n1 -s
+
 clear
 
 bash "/home/$USER/Klipper_Power_Resume/Interface_scripts/menu.sh" restart
