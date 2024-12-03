f7a1e50f56b85b188d71b2a0ae24a8325ddf1f74
T9Air <163940743+T9Air@users.noreply.github.com>
2024-12-02 22:27:54 -0500

    using add_logs script custom for macro

--- kpr-config/get_config_path.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Get script's own directory and move up to the parent directory
-SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
-CONFIG_PATH=$(dirname "$SCRIPT_DIR")
-
-# Output the path
-echo "$CONFIG_PATH"
