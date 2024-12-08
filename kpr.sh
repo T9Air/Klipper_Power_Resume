128096a7284b458cb985e48d8b586f09a118180c
T9Air <163940743+T9Air@users.noreply.github.com>
2024-12-07 22:20:40 -0500

    update script
    will need a beta version to test

--- kpr.sh
+++ kpr.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 
-echo "Klipper_Power_Resume v3.5.0"
+echo "Klipper_Power_Resume v3.5.1"
 echo " "
 echo "Feedback would be greatly appreciated"
 echo " "
 read -r -n1 -s "Press enter to continue..."
 clear
 
-bash "/home/$USER/Klipper_Power_Resume/Interface_scripts/menu.sh" restart
+current_version="v3.6.0"
+kpr="/home/$USER/Klipper_Power_Resume"
+
+# Check for updates
+cd "$kpr"
+git fetch
+
+latest_version=$(git describe --tags $(git rev-list --tags --max-count=1))
+
+if [ "$current_version" != "$latest_version" ]; then
+    echo "A new version ($latest_version) is available."
+    read -r -p "Do you want to update? (Y/n) " response
+    if [[ "$response" =~ ^(Y|y|)$ ]]; then
+        bash "$kpr/update.sh"
+        exit 0
+    fi
+fi
+
+bash "$kpr/Interface_scripts/menu.sh" restart
 exit 0
