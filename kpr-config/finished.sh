dd55edd8b3af4b0430761dbb16e3e6437aea9845
T9Air <163940743+T9Air@users.noreply.github.com>
2025-02-15 23:05:51 -0500

    add multi-printer ability to log files
    the log files did not have the ability to use a specific printer's logs

--- kpr-config/finished.sh
+++ kpr-config/finished.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+printer_path = "$(dirname "$(dirname "$(dirname "$(reallink -f "$0")")")")"
+
 # Echo "Finished" to the static_log file
-echo "Finished" > "/home/$USER/printer_data/config/kpr-config/static_log.txt"
+echo "Finished" > "$printer_path/config/kpr-config/static_log.txt"
--- kpr-config/unlog.sh
+++ kpr-config/unlog.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+printer_path = "$(dirname "$(dirname "$(dirname "$(reallink -f "$0")")")")"
+
 # Clear log.txt and log file path to it
 file_path=$1
-echo "$file_path" > /home/$USER/printer_data/config/kpr-config/static_log.txt
-echo 0 > /home/$USER/printer_data/config/kpr-config/dynamic_log.txt
+echo "$file_path" > $printer_path/config/kpr-config/static_log.txt
+echo 0 > $printer_path/config/kpr-config/dynamic_log.txt
